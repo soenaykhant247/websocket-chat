4f4764985f5d730135732940f0c30ca9d7789cbd
soenaykhant247 <soenaykhantworks247@gmail.com>
2025-04-04 02:36:27 +0700

    Moved index to public and edited server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const mongoose = require("mongoose");
 const socketIo = require("socket.io");
 const cors = require("cors");
+const path = require("path");
 
 // Initialize Express app
 const app = express();
@@ -10,7 +11,7 @@ const server = http.createServer(app);
 const io = socketIo(server, { cors: { origin: "*" } });
 
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://root:<db_password>@testingwebsocket.6omjh71.mongodb.net/?retryWrites=true&w=majority&appName=TestingWebSocket", {
+mongoose.connect("mongodb+srv://root:root@testingwebsocket.6omjh71.mongodb.net/?retryWrites=true&w=majority&appName=TestingWebSocket", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
@@ -25,6 +26,14 @@ const ChatMessage = mongoose.model("ChatMessage", new mongoose.Schema({
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the index.html file at the root URL
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // Fetch chat history API
 app.get("/messages", async (req, res) => {
@@ -46,4 +55,6 @@ io.on("connection", (socket) => {
 });
 
 // Start Server
-server.listen(5001, () => console.log("Server running on port 5000"));
+// server.listen(5001, () => console.log("Server running on port 5001"));
+const PORT = process.env.PORT || 5001;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
